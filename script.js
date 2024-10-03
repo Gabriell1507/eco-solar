1f59aa7e0a3ea6c9e55efb6b1e1700f3ce1be6fd
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2024-10-03 19:10:59 -0300

    subindo arquivos

---
+++ script.js
@@ -0,0 +1,29 @@
+const scrollToTopButton = document.querySelector('.scroll-to-top');
+
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) { 
+    scrollToTopButton.classList.add('show');
+  } else {
+    scrollToTopButton.classList.remove('show');
+  }
+});
+
+
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+
+ const links = document.querySelectorAll('.nav-links a');
+
+
+ const currentPage = window.location.pathname.split("/").pop();
+
+ 
+ links.forEach(link => {
+     if (link.getAttribute('href') === currentPage) {
+         link.classList.add('active-link');
+     }
+ });
